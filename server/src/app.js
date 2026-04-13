5da60fbbe57047f569dd688edd0dc2696b4610d5
Rounak Seth <sethrounak7@gmail.com>
2026-04-13 15:15:35 +0530

    Fix CORS

--- server/src/app.js
+++ server/src/app.js
@@ -5,6 +5,6 @@ import mailRouter from "./route.js";
 
 export const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173","https://my-portfolio-lzkv.onrender.com"], credentials: true }));
 app.use(express.json());
 app.use("/api", mailRouter);
